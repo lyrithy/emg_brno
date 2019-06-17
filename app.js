16a175127816ab19a9c69544d7a05cd13748591d
lyrithy <lyrithyit@gmail.com>
2019-06-17 19:39:12 +0200

    update interface

--- app.js
+++ app.js
@@ -56,6 +56,8 @@ app.use(function(req, res, next) {
 // Routes
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
-const PORT = process.env.PORT || 3000;
 
+
+//App start
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
